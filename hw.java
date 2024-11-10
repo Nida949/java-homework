945c8ea6f2f95f5466a86821a15168a9c23a997f
Nida949 <sknidatabassum@gmail.com>
2024-11-10 20:32:05 +0530

    Create hw.java

---
+++ hw.java
@@ -0,0 +1,14 @@
+Write a program to display the message 'Happy New Year' ten times on the screen.
+import java.util.*;
+class hwpg185q1
+{
+    public static void main ()
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,a=10;
+        for(i=1;i<=a;i++)
+        {
+            System.out.println("Happy New Year");
+        }
+    }
+}
